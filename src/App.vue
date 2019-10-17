2b433950869657c7b1cb7c457f8eee74241489a8
kangbingliu <271838040@qq.com>
2019-10-17 14:02:13 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    {{message}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      message: "hello wrold"
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
